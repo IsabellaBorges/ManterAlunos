3888ebcca1d153f8f3d88a6b83ec5026ff72b346
IsabellaBorges <isasousaah@gmail.com>
2019-02-26 22:19:44 -0300

    MAnterAlunos02

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Aluno } from "../alunos/pessoa";
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  public pessoa: Aluno[];
+
+  constructor (public http:HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get<Aluno[]>('http://gilsonpolito-api.herokuapp.com/alunos')   
+    .subscribe(
+       (aluno)=>{
+         this.pessoa=aluno;
+       }
+    )   
+  }
 
 }
